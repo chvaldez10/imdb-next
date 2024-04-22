612ed4740e164dfe189456413fae1f49a6082333
chvaldez10 <christianvaldez021@gmail.com>
2024-04-21 22:53:04 -0600

    added dark mode switch

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+export default function DarkModeSwitch() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => setMounted(true), []);
+
+  console.log(`them = ${theme}`);
+  console.log(`systemTheme = ${systemTheme}`);
+  console.log(`currentTheme = ${currentTheme}`);
+
+  return (
+    <div>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            onClick={() => setTheme("light")}
+            className="text-xl cursor-pointer hover:text-amber-500"
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme("dark")}
+            className="text-xl cursor-pointer hover:text-amber-500"
+          />
+        ))}
+    </div>
+  );
+}
